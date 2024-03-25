0050c73707f0504d45da61ceda3eba9e92013bb0
Brian Akumah <bakumah@liberty.edu>
2024-03-24 23:26:29 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {Ionicons} from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <View style={styles.row}>
+          <Text style={styles.headerStyle}>Dashboard</Text>
+          <Ionicons name="trash-outline"
+                    style={styles.trashIconStyle}
+                    size={35}/>
+        </View>
+        <StatusBar style="auto"/>
+      </View>
   );
 }
 
@@ -15,6 +21,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 50,
+  },
+  headerStyle: {
+    color: '#000',
+    fontSize: 30,
+    textAlign: 'center', // Align text in the center horizontally
+    flex: 1, // Expand to fill available space
+  },
+  trashIconStyle: {
+    color: '#000',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center', // Align items vertically in the center
+    width: '100%', // Ensures the row spans the entire width of the container
+    paddingHorizontal: 20, // Adds some padding to the sides of the row
   },
 });
